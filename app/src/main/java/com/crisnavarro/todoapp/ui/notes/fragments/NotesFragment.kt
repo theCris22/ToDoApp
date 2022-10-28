9a6b95e11a528eaf8c42440fa339e833a222e0ad
Cristian Navarro Gonzalez <cristianskate82@hotmail.com>
2022-10-28 11:37:44 -0500

    NotesApp. Update notes design, and add menus to fragments

--- app/src/main/java/com/crisnavarro/todoapp/ui/notes/fragments/NotesFragment.kt
+++ app/src/main/java/com/crisnavarro/todoapp/ui/notes/fragments/NotesFragment.kt
@@ -1,15 +1,15 @@
 package com.crisnavarro.todoapp.ui.notes.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.crisnavarro.todoapp.R
 import com.crisnavarro.todoapp.databinding.FragmentNotesBinding
 
-class NotesFragment : Fragment(R.layout.fragment_notes) {
+class NotesFragment : Fragment(R.layout.fragment_notes), MenuProvider {
 
     private var binding: FragmentNotesBinding? = null
 
@@ -29,13 +29,15 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
     }
 
 
-    private fun setUpViews() {}
+    private fun setUpViews() {
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
 
     private fun setUpListener() {
-
         with(binding!!) {
 
-            fabAddNote.setOnClickListener { goToAddNote() }
+            fabAddNote.setOnClickListener { goToUpdateNote() }
+            recyclerViewNotes.setOnClickListener { goToUpdateNote() }
 
         }
     }
@@ -43,6 +45,26 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
     private fun goToAddNote() {
         val navigate = NotesFragmentDirections.actionNotesFragmentToAddNoteFragment()
         findNavController().navigate(navigate)
+    }
+
+    private fun goToUpdateNote() {
+        val navigate = NotesFragmentDirections.actionNotesFragmentToUpdateNoteFragment()
+        findNavController().navigate(navigate)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_notes_fragment, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        /*return when (menuItem.itemId) {
+            R.id.clear_text_menu -> {
+                binding!!.editTextHash.text.clear()
+                true
+            }
+            else -> false
+        }*/
+        return false
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/crisnavarro/todoapp/ui/updatenotes/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/crisnavarro/todoapp/ui/updatenotes/fragments/UpdateNoteFragment.kt
@@ -1,7 +1,53 @@
 package com.crisnavarro.todoapp.ui.updatenotes.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import com.crisnavarro.todoapp.R
+import com.crisnavarro.todoapp.databinding.FragmentUpdateNoteBinding
 
-class UpdateNoteFragment : Fragment() {
+class UpdateNoteFragment : Fragment(R.layout.fragment_update_note), MenuProvider {
+
+    private var binding: FragmentUpdateNoteBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUpdateNoteBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.update_note_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.icon_delete -> {
+                Log.e("DELETE", "DELETE")
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 
 }
